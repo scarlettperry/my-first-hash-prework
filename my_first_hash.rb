48b2cd8ec8ee5d9022ca5c591f3fe8abd89dc8b1
Scarlett Perry <si.perry15@gmail.com>
2018-07-03 14:23:13 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,10 +26,12 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+    
+  }
+  shipping_manifest[""] = 2 
   # add 2 muskets to the shipping_manifest hash below
 
 
