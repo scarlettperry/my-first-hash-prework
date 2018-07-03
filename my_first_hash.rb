a5fa1f6db7c656b03c9bc2b7310a82e91befbba9
Scarlett Perry <si.perry15@gmail.com>
2018-07-03 14:23:35 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -31,12 +31,8 @@ def adding
     "oil paintings" => 3
     
   }
-  shipping_manifest[""] = 2 
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
+  shipping_manifest["muskets"] = 2
+  shipping_manifest[""] = 4
 
 
 
